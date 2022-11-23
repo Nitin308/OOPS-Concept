49538ebaba4f1a78fa1c8ae83b55835d04a14392
Nitin308 <114205043+Nitin308@users.noreply.github.com>
2022-11-23 21:35:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        EmpWageBuilderObject Microsoft = new EmpWageBuilderObject("Microsoft", 100, 20, 100);
+        EmpWageBuilderObject Google = new EmpWageBuilderObject("Google", 200, 30, 150);
+        Microsoft.computeEmpWage();
+        Console.WriteLine(Microsoft.toString());
+        Google.computeEmpWage();
+        Console.WriteLine(Google.toString());
+        Console.ReadLine();
+    }
+}
