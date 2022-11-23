53f7ff8a7242747c2336ebd931316b879a577385
Nitin308 <114205043+Nitin308@users.noreply.github.com>
2022-11-23 21:24:22 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    public const int IS_FULL_TIME = 1;
+    public const int IS_PART_TIME = 2;
+    public const int EMP_RATE_PER_HOUR = 100;
+    public const int NUM_OF_WORKING_DAYS = 20;
+    static void Main(string[] args)
+    {
+        int empHrs = 0;
+        int empWage = 0;
+        int totalEmpWage = 0;
+        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpWage += empWage;
+            Console.WriteLine("Employee Wage: " + empWage);
+        }
+        Console.WriteLine("Total Employee Wage: " + totalEmpWage);
+        Console.ReadLine();
+    }
+}
